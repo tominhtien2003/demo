69a612241892d2163b3068e7360f41e6ca98296e
tominhtien2003 <totiendepzai@gmail.com>
2023-02-03 12:16:00 +0700

    github cơ bản

---
+++ githup.cpp
@@ -0,0 +1,10 @@
+/*
+--git init : Khởi tạo 1 git repository , 1 project mới hoặc đã có.
+--git clone : Copy 1 git repository từ remote source.(Láy từ trên mạng về);
+--git pull : đồng bộ từ trên magj vè máy
+--git add và git add : 
+--git commit
+--git push : đồng bộ từ máy lên repository
+--git log
+--gitlog--decorate--graph--oneline
+*/
