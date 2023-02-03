ab34fedcf8b57a797bd95ce6664696dd6e11dc80
tominhtien2003 <totiendepzai@gmail.com>
2023-02-03 12:30:29 +0700

    compare

---
+++ compare.cpp
@@ -0,0 +1,22 @@
+// template< class T, class Alloc >
+// bool operator==( const std::vector<T,Alloc>& lhs,
+//                  const std::vector<T,Alloc>& rhs );
+// template< class T, class Alloc >
+// constexpr bool operator==( const std::vector<T,Alloc>& lhs,
+//                            const std::vector<T,Alloc>& rhs );
+// template< class T, class Alloc >
+// bool operator!=( const std::vector<T,Alloc>& lhs,
+//                  const std::vector<T,Alloc>& rhs );
+// template< class T, class Alloc >
+// bool operator<( const std::vector<T,Alloc>& lhs,
+//                 const std::vector<T,Alloc>& rhs );
+// template< class T, class Alloc >
+// bool operator<=( const std::vector<T,Alloc>& lhs,
+//                  const std::vector<T,Alloc>& rhs );
+// template< class T, class Alloc >
+// bool operator>( const std::vector<T,Alloc>& lhs,
+//                 const std::vector<T,Alloc>& rhs );
+// template< class T, class Alloc >
+// bool operator>=( const std::vector<T,Alloc>& lhs,
+//                  const std::vector<T,Alloc>& rhs );
+
